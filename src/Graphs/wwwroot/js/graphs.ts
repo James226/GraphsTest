43ec6381a1965ff8fed1c36f0239b4ae57fe3d5c
James Parker <james.parker@esendex.com>
2015-06-29 21:18:04 +0100

    Initial Commit

---
+++ src/Graphs/wwwroot/js/graphs.ts
@@ -0,0 +1,34 @@
+﻿module Graphs {
+    export var app = window["angular"].module("graphs", []);
+
+    class HomeController {
+        constructor($scope) {
+            $scope.test = "Test";
+        }
+    }
+    app.controller("HomeController", ["$scope", HomeController]);
+
+    app.factory('d3Service', ['$document', '$q', '$rootScope',
+        function ($document, $q, $rootScope) {
+            var d = $q.defer();
+            function onScriptLoad() {
+                $rootScope.$apply(function () { d.resolve(window["d3"]); });
+            }
+
+            var scriptTag = $document[0].createElement('script');
+            scriptTag.type = 'text/javascript';
+            scriptTag.async = true;
+            scriptTag.src = '/Scripts/d3.min.js';
+            scriptTag.onreadystatechange = function () {
+                if (this.readyState == 'complete') onScriptLoad();
+            }
+            scriptTag.onload = onScriptLoad;
+
+            var s = $document[0].getElementsByTagName('body')[0];
+            s.appendChild(scriptTag);
+
+            return {
+                d3: function () { return d.promise; }
+            };
+        }]);
+}
